a0424dd365c18e110eea5b3dabfb0b967080f237
lihangyu1913092 <2415358701@qq.com>
2022-05-19 00:15:11 +0800

    add views

---
+++ virtualMachineMonitoringSystemBackend/views/user_views.py
@@ -0,0 +1,15 @@
+from django.http import JsonResponse
+
+
+def login(request):
+    # TODO
+    return JsonResponse({
+        'isSuccess': True
+    })
+
+
+def register(request):
+    # TODO
+    return JsonResponse({
+        'isSuccess': True
+    })
