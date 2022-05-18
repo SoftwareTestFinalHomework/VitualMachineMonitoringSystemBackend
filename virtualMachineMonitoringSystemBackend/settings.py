849155d28c25bbb8331da42881b6be5de15290c7
lihangyu1913092 <2415358701@qq.com>
2022-05-19 00:15:32 +0800

    modify

--- virtualMachineMonitoringSystemBackend/settings.py
+++ virtualMachineMonitoringSystemBackend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-tbo='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -68,10 +68,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'virtualMachineMonitoringSystemBackend.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
--- virtualMachineMonitoringSystemBackend/urls.py
+++ virtualMachineMonitoringSystemBackend/urls.py
@@ -13,9 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
+from virtualMachineMonitoringSystemBackend.views import user_views, vitual_machine_views, server_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('login', user_views.login, name='login'),
+    path('register', user_views.register, name='register'),
+    path('getCPUUsage', vitual_machine_views.get_cpu_usage, name='getCPUUsage')
 ]
